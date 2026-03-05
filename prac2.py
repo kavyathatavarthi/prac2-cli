2ecd695521f338f0e9844a61d1807c5300d0e150
kavyathatavarthi <kavyathatavarthi@gmail.com>
2026-03-06 00:46:37 +0530

    Update prac2.py

--- prac2.py
+++ prac2.py
@@ -1,2 +1,13 @@
-printf("hello world")
-printf("change in feature branch")
+import sys
+def add_numbers(a, b):
+ return a + b
+if __name__ == "__main__":
+ num1 = int(sys.argv[1])
+ num2 = int(sys.argv[2])
+ result = add_numbers(num1, num2)
+ print("=================================")
+ print("Addition Result")
+ print("=================================")
+ print(f"First Number : {num1}")
+ print(f"Second Number: {num2}")
+ print(f"Sum : {result}")
